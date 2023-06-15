4f6259e3aeb015e2bfcb04bf785a5a0076896520
Daiena <daiena@mail.ru>
2023-06-15 14:55:12 +0300

    добавлен useReducer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import MyButton from "../components/MyButton";
 import Profile from "../components/Profile";
 import Product, { IProduct } from "../components/Product";
@@ -7,6 +7,7 @@ import Counter from "../components/Counter";
 import { useState } from 'react';
 import MyInput from '../components/MyInput';
 import MyForm from '../components/MyForm';
+import { productReducer } from "../store/productReducer";
 
 const initialState=[
     {id:1, count:1, name:"Бананы", inCart:false, price:1000},
@@ -14,19 +15,31 @@ const initialState=[
 
 const HomePage=()=>{
 
-const [productList, setProductList]=useState<IProduct[]>(initialState);
-
+// const [productList, setProductList]=useState<IProduct[]>(initialState);
+const [productList, dispatch]=useReducer(productReducer, initialState);
 const addProduct=(product:IProduct)=>{
-  product.id=Math.max(...productList.map(p=>p.id), 0)+1;
-  setProductList([...productList, product]);
+  dispatch({
+    type: "add",
+    payload: product,
+  })
+  // product.id=Math.max(...productList.map(p=>p.id), 0)+1;
+  // setProductList([...productList, product]);
 };
 
 const updateProduct=(product:IProduct):void=>{
-  setProductList(productList.map(p =>p.id === product.id? product : p))
+  dispatch({
+    type: "update",
+    payload: product,
+  })
+  // setProductList(productList.map(p =>p.id === product.id? product : p))
 }
 
 const deleteProduct=(product:IProduct):void=>{
-  setProductList(productList.filter(p =>p.id !== product.id))
+  dispatch({
+    type: "delete",
+    payload: product,
+  })
+  // setProductList(productList.filter(p =>p.id !== product.id))
 }
     return(
     <>
