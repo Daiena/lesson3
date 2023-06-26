59adce5faa1963c391b23ef428608175e9227267
Daiena <daiena@mail.ru>
2023-06-26 10:03:30 +0300

    фокус на инпут с обычной кнопкой и инпутом

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,44 +1,15 @@
-import { log } from 'console';
-import { useRef, useState } from 'react';
+import React, { useRef } from "react";
 
-export default function Chat() {
-  const [text, setText] = useState('');
-  const [isSending, setIsSending] = useState(false);
-  const timeoutID = useRef<any>(null);
-
-  function handleSend() {
-    setIsSending(true);
-    timeoutID.current = setTimeout(() => {
-      alert('Sent!');
-      setIsSending(false);
-    }, 3000);
-    console.log("Нажали отправить", timeoutID.current);
-    
-  }
-
-  function handleUndo() {
-    console.log("Нажали отмена", timeoutID.current);
-    setIsSending(false);
-    clearTimeout(timeoutID.current);
-  }
-
-  return (
-    <>
-      <input
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button
-        disabled={isSending}
-        onClick={handleSend}>
-        {isSending ? 'Sending...' : 'Send'}
-      </button>
-      {isSending &&
-        <button onClick={handleUndo}>
-          Undo
-        </button>
-      }
-    </>
-  );
+const TestPage=()=>{
+    const inputRef=useRef<HTMLInputElement>(null);
+    const handleClick=()=>{
+        inputRef.current?.focus();
+    }
+    return(
+        <div>
+            <input type="text" ref={inputRef} />
+            <button onClick={handleClick}>Focus</button>
+        </div>
+    )
 }
+export default TestPage;
