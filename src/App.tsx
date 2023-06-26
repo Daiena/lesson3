5589fd09f7f53ff90e5bf41c5aec36c3c2a5248f
Daiena <daiena@mail.ru>
2023-06-26 14:45:49 +0300

    исходный список из 20 товаров

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 import TestPage from './pages/TestPage';
 
 function App() {
   return(
     <div className='container'>
-    <TestPage/>
+    <HomePage />
     </div>
   )
 };
