fea82e13d902ba86aaed8c7dbbb7022d2fabe9b9
Daiena <daiena@mail.ru>
2023-06-27 13:44:59 +0300

    useEffect для получения постов и отписки/действой

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MyVideoPlayer from "../components/VideoPlayer/MyVideoPlayer"
+import MyInput from "../components/MyInput";
 
 
 const TestPage=()=>{
-   
+    const [value, setValue]=useState("");
+    const [posts, setPosts]=useState();
+    const buttonRef=useRef<HTMLButtonElement>(null);
+    const getPosts=async ()=>{
+        const response= await fetch ("https://jsonplaceholder.typicode.com/posts");
+        const data= await response.json();
+        console.log(data);        
+    }  
+    useEffect(()=>{
+        getPosts();
+        const handleClick=()=>{alert("кнопка нажата")};
+        buttonRef.current?.addEventListener("click", handleClick);
+        return ()=>{
+            buttonRef.current?.removeEventListener("click", handleClick);
+        }
+    }, []) 
+  
+    const handleChange:React.ChangeEventHandler<HTMLInputElement>=(e)=>{
+        setValue(e.target.value);
+    }
     return(
         <div className="row">
+              
             <div className="col-8">
-             <MyVideoPlayer />
+            <button ref={buttonRef}>Кнопка</button>
+            <MyInput value={value} name="test" handleChange={handleChange} />
             </div>
         </div>
     )
